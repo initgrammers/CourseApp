561bcc62103ef02a083240de0b349c48544f604e
Initgrammers <initgrammers@gmail.com>
2019-12-18 10:19:54 -0500

    Initial commit

---
+++ App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import AppEntry from "./App/index";
+export default function App() {
+  return <AppEntry />;
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  }
+});
